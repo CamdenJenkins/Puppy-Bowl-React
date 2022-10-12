8dcdee27dc16fa6280e6bf19961eed56d7191ea1
Camden Jenkins <camdenjenkins@yahoo.com>
2022-10-12 15:09:32 -0700

    NewDogForm

--- src/components/NewDogForm.js
+++ src/components/NewDogForm.js
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import makePuppy from "../ajaxHelpers/makePuppy";
 
+const NewDogForm = () => {
+  const navigate = useNavigate;
 
-const NewDogForm = 
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+
+  return (
+    <div>
+      <form
+        className="pure-form pure-form-stacked"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const result = await makePuppy(name, breed);
+          navigate("/");
+        }}
+      ></form>
+    </div>
+  );
+};
